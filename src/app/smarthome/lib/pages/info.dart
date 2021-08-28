2a7f76a3e6a54f728073e5094b6b632c0e6fffd3
Jan Bellenberg <janbellenberg.jb@gmail.com>
2021-08-28 14:42:39 +0200

    Fixed & refactored info page

--- src/app/smarthome/lib/pages/info.dart
+++ src/app/smarthome/lib/pages/info.dart
@@ -26,8 +26,10 @@ class InfoPage extends StatelessWidget {
                       size: 30.0,
                     ),
                   ),
-                  Text("Über Smarthome",
-                      style: TextStyle(color: WHITE, fontSize: 30.0))
+                  Text(
+                    "Über Smarthome",
+                    style: TextStyle(color: WHITE, fontSize: 30.0),
+                  )
                 ],
               ),
             ),
@@ -47,59 +49,73 @@ class InfoPage extends StatelessWidget {
                     padding: const EdgeInsets.only(bottom: 20.0),
                     child: Column(
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.all(50.0),
-                          child: Container(
-                            padding: const EdgeInsets.only(
-                                top: 40.0,
-                                bottom: 40.0,
-                                left: 50.0,
-                                right: 50.0),
-                            decoration: BoxDecoration(
-                                color: Theme.of(context).backgroundColor,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Theme.of(context)
-                                        .accentColor
-                                        .withOpacity(0.28),
-                                    spreadRadius: 0,
-                                    blurRadius: 15,
-                                    offset: Offset(0, 0),
-                                  ),
-                                ],
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(500.0))),
-                            child: Image.asset(
-                              "assets/images/icon_inverted.png",
-                              width: 100.0,
+                        Container(
+                          margin: const EdgeInsets.all(50.0),
+                          padding: const EdgeInsets.only(
+                            top: 40.0,
+                            bottom: 40.0,
+                            left: 50.0,
+                            right: 50.0,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).backgroundColor,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Theme.of(context)
+                                    .accentColor
+                                    .withOpacity(0.28),
+                                spreadRadius: 0,
+                                blurRadius: 15,
+                                offset: Offset(0, 0),
+                              ),
+                            ],
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(500.0),
                             ),
+                          ),
+                          child: Image.asset(
+                            "assets/images/icon_inverted.png",
+                            width: 100.0,
                           ),
                         ),
                         FutureBuilder(
-                            future: rootBundle.loadString("pubspec.yaml"),
-                            builder: (context, snapshot) {
-                              String version = "Unknown";
-                              if (snapshot.hasData) {
-                                var yaml = loadYaml(snapshot.data.toString());
-                                version = yaml["version"];
-                              }
-
-                              return Text('Version: $version',
-                                  style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.bold));
-                            }),
+                          future: rootBundle.loadString("pubspec.yaml"),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Text(
+                                'Version: ' +
+                                    loadYaml(
+                                      snapshot.data.toString(),
+                                    )["version"]
+                                        .toString()
+                                        .split("+")[0],
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              );
+                            }
+                            return Container();
+                          },
+                        ),
                         Padding(
                           padding: const EdgeInsets.all(5.0),
-                          child: Text("Copyright Ⓒ Jan Bellenberg 2021",
-                              style: TextStyle(fontSize: 15.0)),
+                          child: Text(
+                            "Copyright Ⓒ Jan Bellenberg 2021",
+                            style: TextStyle(fontSize: 15.0),
+                          ),
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Text("Made with", style: TextStyle(fontSize: 15.0)),
+                            Text(
+                              "Made with",
+                              style: TextStyle(fontSize: 15.0),
+                            ),
                             FlutterLogo(
-                                size: 150.0, style: FlutterLogoStyle.horizontal)
+                              size: 150.0,
+                              style: FlutterLogoStyle.horizontal,
+                            )
                           ],
                         ),
                       ],
