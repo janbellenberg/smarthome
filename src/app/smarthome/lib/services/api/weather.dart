db241c51742f778ac0615f24335a670eea6989cd
Jan Bellenberg <jan.bellenberg@outlook.de>
2022-02-08 12:20:55 +0100

    Fixed weather loading

--- src/app/smarthome/lib/services/api/weather.dart
+++ src/app/smarthome/lib/services/api/weather.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart';
 dynamic loadWeather(String city, String country) async {
   try {
     Uri url = Uri.parse(
-      'http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${WEATHER_API_KEY}',
+      'https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${WEATHER_API_KEY}',
     );
 
     Response res = await get(url);
